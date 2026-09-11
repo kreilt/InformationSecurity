e97104304ff0d17f171f274c5a46a59dfcff1bb2
kreilt <pogodin-olezhka@inbox.ru>
2026-09-11 21:23:35 +0300

    2 version decoder

--- whitstonDS.go
+++ whitstonDS.go
@@ -23,25 +23,41 @@ var replacer = strings.NewReplacer(
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	line, err := reader.ReadString('\n')
-	if err != nil && line == "" {
-		fmt.Println("Ошибка чтения", err)
-		return
-	}
-
-	bigrams := splitByTwoRunes(line)
+	bigrams := splitByTwoRunes(input())
 
 	m1 := randMatrix()
 	m2 := randMatrix()
+	m1, m2 = refMatrices()
+	printMatrices(m1, m2)
 
-	fmt.Println("Алфавит №1")
-	printMatrix(m1)
-	fmt.Println("Алфавит №2")
-	printMatrix(m2)
-
+	fmt.Println("\nШИФРУЕМ")
 	encrypted := handlerCoder(bigrams, m1, m2)
+
+	fmt.Println("\nРАСШИФРОВЫВАЕМ")
 	handlerDecoder(encrypted, m1, m2)
+	handlerCoder(encrypted, m2, m1)
+}
+
+func input() string {
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	if err != nil && line == "" {
+		fmt.Fprintf(os.Stderr, "ошибка чтения строки: %v\n", err)
+	}
+	return line
+}
+
+func splitByTwoRunes(line string) [][]rune {
+	line = replacer.Replace(strings.ToUpper(line))
+	rs := []rune(line)
+	if len(rs)%2 != 0 {
+		rs = append(rs, ' ')
+	}
+	res := make([][]rune, 0, len(rs)/2)
+	for i := 0; i < len(rs); i += 2 {
+		res = append(res, rs[i:i+2])
+	}
+	return res
 }
 
 func randMatrix() [][]rune {
@@ -63,21 +79,7 @@ func randMatrix() [][]rune {
 	return m
 }
 
-func splitByTwoRunes(line string) [][]rune {
-	line = replacer.Replace(strings.ToUpper(line))
-	rs := []rune(line)
-	if len(rs)%2 != 0 {
-		rs = append(rs, 'Ъ')
-	}
-	res := make([][]rune, 0, len(rs)/2)
-	for i := 0; i < len(rs); i += 2 {
-		res = append(res, rs[i:i+2])
-	}
-	return res
-}
-
 func handlerCoder(bigrams [][]rune, m1, m2 [][]rune) [][]rune {
-	fmt.Println("\nШИФРУЕМ")
 
 	out := make([][]rune, 0, len(bigrams))
 	for _, bg := range bigrams {
@@ -90,11 +92,10 @@ func handlerCoder(bigrams [][]rune, m1, m2 [][]rune) [][]rune {
 }
 
 func handlerDecoder(bigrams [][]rune, m1, m2 [][]rune) [][]rune {
-	fmt.Println("\nРАСШИФРОВЫВАЕМ")
 
 	out := make([][]rune, 0, len(bigrams))
 	for _, bg := range bigrams {
-		c := coder(bg, m2, m1)
+		c := decoder(bg, m1, m2)
 		out = append(out, c)
 		fmt.Printf("%q ", string(c))
 	}
@@ -129,16 +130,90 @@ func coder(bigram []rune, m1, m2 [][]rune) []rune {
 	return out
 }
 
-func printMatrix(m [][]rune) {
-	for _, row := range m {
-		for _, r := range row {
-			if r == ' ' {
-				fmt.Print("_ ") // иначе пробел выглядит как потерянная ячейка
-			} else {
-				fmt.Printf("%c ", r)
+func decoder(bigram []rune, m1, m2 [][]rune) []rune {
+	var i1, j1 int = -1, -1
+	var i2, j2 int = -1, -1
+	var i1alter, j1alter int = -1, -1
+	var i2alter, j2alter int = -1, -1
+	out := make([]rune, 2)
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if bigram[0] == m2[i][j] {
+				i1 = i
+				j1 = j
+			}
+			if bigram[1] == m1[i][j] {
+				i2 = i
+				j2 = j
+			}
+			if bigram[0] == m1[i][j] {
+				i1alter = i
+				j1alter = j
+			}
+			if bigram[1] == m2[i][j] {
+				i2alter = i
+				j2alter = j
 			}
 		}
+	}
+	if i1 == -1 || i2 == -1 || j1 == -1 || j2 == -1 {
+		panic("Проблема шифрования")
+	}
+	if i1alter == i2alter {
+		out[0], out[1] = m1[i1alter][j2alter], m2[i2alter][j1alter]
+	} else {
+		out[0], out[1] = m1[i1][j2], m2[i2][j1]
+	}
+	return out
+}
+
+func printMatrices(m1, m2 [][]rune) {
+	fmt.Println("   Алфавит №1     Алфавит №2")
+	for i := 0; i < rows; i++ {
+		fmt.Print("   ")
+		printRow(m1[i])
+		fmt.Print("     ")
+		printRow(m2[i])
 		fmt.Println()
 	}
 	fmt.Println()
+}
+
+func printRow(row []rune) {
+	for _, r := range row {
+		if r == ' ' {
+			fmt.Print("_ ")
+		} else {
+			fmt.Printf("%c ", r)
+		}
+	}
+}
+
+func refMatrices() ([][]rune, [][]rune) {
+	parse := func(rowsStr []string) [][]rune {
+		m := make([][]rune, rows)
+		for i, s := range rowsStr {
+			m[i] = []rune(s)
+		}
+		return m
+	}
+	left := parse([]string{
+		"ЖЩНЮР",
+		"ИТЬЦБ",
+		"ЯМЕ.С",
+		"ВЫПЧ ",
+		":ДУОК",
+		"ЗЭФГШ",
+		"ХА,ЛЪ",
+	})
+	right := parse([]string{
+		"ИЧГЯТ",
+		",ЖЬМО",
+		"ЗЮРВЩ",
+		"Ц:ПЕЛ",
+		"ЪАН.Х",
+		"ЭКСШД",
+		"БФУЫ ",
+	})
+	return left, right
 }
